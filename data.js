152fd8d05594f6497dc516e29dd0e91212cf5dd2
ktb702 <59974874+ktb702@users.noreply.github.com>
2020-07-11 15:20:22 -0700

    changed directory structure

---
+++ data.js
@@ -0,0 +1,21 @@
+/////////////////////////////////////////////////////////
+// Author: Kate Baldwin
+// Course: IT122  (Week 1 - Node.js Up and Running)
+// File: data.js
+// Created: 07/04/2020
+// Desc: Define an array with at least 5 items and export
+/////////////////////////////////////////////////////////
+
+// create an array of car objects/items
+let cars = [
+    {year: 2009, make: 'Audi',   model: 'A4',      color: 'black'},
+    {year: 2016, make: 'Toyota', model: 'Prius',   color: 'silver'},
+    {year: 2018, make: 'Mini',   model: 'Cooper',  color: 'purple'},
+    {year: 2012, make: 'Subaru', model: 'Outback', color: 'blue'},
+    {year: 2010, make: 'Ford',   model: 'Mustang', color: 'red'},
+];
+
+// export method that will return all array (cars) items
+exports.getAll = () => {
+    return cars;
+}
