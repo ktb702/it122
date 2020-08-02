4fa0c40aba520cc5eb864a08237a3137fa5f1d73
ktb702 <59974874+ktb702@users.noreply.github.com>
2020-08-02 12:57:40 -0700

    all 6 test cases working

--- test/carsTest.js
+++ test/carsTest.js
@@ -11,21 +11,59 @@
 const expect = require("chai").expect;
 const car = require("../data.js"); //modules being tested
 
-//test cases for success and failure conditions for getItem method
 describe('Testing Cars Module (data.js)', () => {
+    //test cases for success and failure conditions for getItem method
     describe("get car item", () => {
         it("returns requested car", () => {
             const result = car.getItem("Prius");
-            expect(result).to.deep.equal({year: 2016, make:"Toyota", model: "Prius", color: "silver"});
+            expect(result.car).to.deep.equal({year: 2016, make:"Toyota", model: "Prius", color: "silver"});
           });
           
-          it("fails w/ invalid car", () => {
+          it("fails with invalid car", () => {
             const result = car.getItem("monster truck");
-            expect(result).to.be.undefined;
+            expect(result.status).to.be.false;
           });
+    });
+
+    //test cases for success and failure conditions for addItem method
+    describe("add car", () => {
+        it("should add the new car to the array", () => {
+            let newCar = {
+                year: 2014,
+                make: "Jeep",
+                model: "Wrangler",
+                color: "green"
+            };
+            const result = car.addItem(newCar);
+            expect(result.status).to.be.true;
+          });
+
+          it("fails if data already exists in array", () => {
+            let newCar = {
+                year: 2010,
+                make: "Ford",
+                model: "Mustang",
+                color: "red"
+            }; 
+            const result = car.addItem(newCar);
+            expect(result.status).to.be.false;
+          });
+    }); 
+
+    //test cases for success and failure conditions for deleteItem method
+    describe("delete car", () => {
+        it("should delete a car from the array", () => {
+            let result = car.deleteItem("Cooper");
+            expect(result.status).to.be.true;
+        });
+
+        it("fails if data doesn't exist in the array", () => {
+          let result = car.deleteItem("Ferrari");
+          expect(result.status).to.be.false;
+      });
     });
    });
 
-//test cases for success and failure conditions for addItem method
 
-//test cases for success and failure conditions for deleteItem method
+
+
